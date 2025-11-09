10d27a040db8329c0e5fb686eebbb9ce078885b9
Fangxun Liu <cz2128@ascend-rw02.ten.osc.edu>
2025-11-09 15:51:23 -0500

    Skip MViT if there is import error

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 from .build import MODEL_REGISTRY, build_model
 from .uniformerv2 import Uniformerv2
 from .uniformerv2_reid import Uniformerv2ReID
-from .mvitv2_reid import MViTReID
 from .videomaev2_reid import VideoMAEv2ReID
+
+try:
+    from .mvitv2_reid import MViTReID  # noqa: F401
+except ImportError:
+    # Optional dependency (SlowFast + PyTorchVideo) not installed; skip registration.
+    MViTReID = None  # type: ignore
